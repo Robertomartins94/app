c36f1181927832bfb0fafcde91c4055d12199c28
Roberto-Martins <robertomartins9402@gmail.com>
2024-09-09 22:17:04 -0300

    fundamentals 003

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // arrays, objects
 let meta = {
   value: "ler um livro por mês",
-  address: 2,
   checked: true,
-  log: (info) => {
-      console.log(info)
-  }
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+  meta, 
+  {
+      value: "caminhar 20 minutos todos os dias",
+      checked: false
+  }
+]
+
+console.log(metas[1].value)
 
 // function // arrow function
 //const criarMeta = () => {}
